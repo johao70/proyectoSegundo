db7c8b5aaf35c2b303f75824770d16329ce2d463
johao70 <perlazajohao@gmail.com>
2019-01-10 08:32:55 -0500

    bd

--- src/app/database/modulo/class.conexion.php
+++ src/app/database/modulo/class.conexion.php
@@ -1,12 +1,12 @@
 <?php
-    class Conexion{
-        public function get_conexion(){
-            $user = "postgres";
-            $pass = "123";
-            $host = "localhost";
-            $db = "DB_reservaMesas"
-            $conexion = new PDO('postgres:host = $host;dbname = $db;', $user, $pass);
-            return $conexion
-        }
+class Conexion{
+    public function get_conexion(){
+        $user = "postgres";
+        $pass = "123";
+        $host = "localhost";
+        $db = "DB_reservaMesas";
+        $conexion = new PDO ("pgsql:host = $host; db = $db;", $user, $pass);
+        return $conexion;
     }
+}
 ?>
--- src/app/database/modulo/class.tablapersona.php
+++ src/app/database/modulo/class.tablapersona.php
@@ -1,22 +1,22 @@
 <?php
     class Tablapersona{
-        public function insertarDatos($pers_numero_identificacion, $pers_nombre, $pers_telefono, $pers_correo, $pers_clave, $pers_fecha_nacimiento, $pers_tipo){
+        public function insertarDatos($pers_numero_identificacion, $pers_nombre, $pers_telefono, $pers_correo, $pers_clave, $pers_fecha_nacimiento, $tipo_pers_sec){
             $modelo = new Conexion();
             $conexion = $modelo -> get_conexion();
-            $sql = "insert into persona(pers_numero_identificacion, pers_nombre, pers_telefono, pers_correo, pers_clave, pers_fecha_nacimiento, pers_tipo) values (
-                :pers_numero_identificacion, :pers_nombre, :pers_telefono, :pers_correo, :pers_clave, :pers_fecha_nacimiento, :pers_tipo)";
+            $sql = "insert into persona(pers_numero_identificacion, pers_nombre, pers_telefono, pers_correo, pers_clave, pers_fecha_nacimiento, tipo_pers_sec) values (
+                :pers_numero_identificacion, :pers_nombre, :pers_telefono, :pers_correo, :pers_clave, :pers_fecha_nacimiento, :tipo_pers_sec)";
             $statement = $conexion -> prepare($sql);
-            $statement -> bindParam(":pers_numero_identificacion");
-            $statement -> bindParam(":pers_nombre");
-            $statement -> bindParam(":pers_telefono");
-            $statement -> bindParam(":pers_correo");
-            $statement -> bindParam(":pers_clave");
-            $statement -> bindParam(":pers_fecha_nacimiento");
-            $statement -> bindParam(":pers_tipo");
+            $statement -> bindParam(":pers_numero_identificacion",$pers_numero_identificacion);
+            $statement -> bindParam(":pers_nombre",$pers_nombre);
+            $statement -> bindParam(":pers_telefono",$pers_telefono);
+            $statement -> bindParam(":pers_correo",$pers_correo);
+            $statement -> bindParam(":pers_clave",$pers_clave);
+            $statement -> bindParam(":pers_fecha_nacimiento",$pers_fecha_nacimiento);
+            $statement -> bindParam(":tipo_pers_sec",$tipo_pers_sec);
             if(!$statement){
                 return "Error al insertar los datos";
             }else{
-                $statement = -> execute();
+                $statement -> execute();
                 return "Datos insertos correctamente";
             }
         }
@@ -24,13 +24,13 @@
         public function consultaDatos(){
             $array = null;
             $modelo = new Conexion();
-            $conexion = $modelo -> get_conexion():
+            $conexion = $modelo -> get_conexion();
             $sql = "select * from persona";
             $statement = $conexion -> prepare($sql);
             $statement -> execute();
             while($resultado = $statement -> fetch());
                 $array = $resultado;
-        }
-        return $array;
+        }        
     }
+    return $array;
 ?>
