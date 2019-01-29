7ea30b12c734175d37319fb158481fccaf95896c
DJRICHARD15 <rlv.tonato@yavirac.edu.ec>
2019-01-29 18:52:47 -0500

    mira

---
+++ .history/Restaurante/src/app/services/reservas.services_20190129184821.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpHeaders, } from '@angular/common/http';
+import { Injectable, Observable } from '@angular/core';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import {  res } from '../models/reserva';
+
+
+
+
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-type': 'aplication/json' })
+};
+const API_URL_CREATE = 'http://localhost:8000/reserva/create';
+@Injectable( )
+
+export class ReservasService {
+    apiUrl: string;
+    constructor(private http: HttpClient) { }
+
+
+
+    crearOActualizar(reservas: Reservas): Observable.Observable<Reservas[]> {
+        return this.http.post<Reservas[]>(API_URL_CREATE, reservas);
+    }
+}
---
+++ .history/Restaurante/src/app/services/reservas.services_20190129185103.ts
@@ -0,0 +1,22 @@
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import {  Reserva } from '../models/reserva';
+
+
+
+
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-type': 'aplication/json' })
+};
+const API_URL_CREATE = 'http://localhost:8000/reserva/create';
+export class ReservasService {
+    apiUrl: string;
+    constructor(private http: HttpClient) { }
+
+
+
+    crearOActualizar(reservas: Reservas): Observable.Observable<Reservas[]> {
+        return this.http.post<Reservas[]>(API_URL_CREATE, reservas);
+    }
+}
