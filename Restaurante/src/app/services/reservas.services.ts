dd42c2ddfc93126a9b24d01da57a37dec8f2f25a
johao70 <perlazajohao@gmail.com>
2019-01-29 19:31:51 -0500

    cambios

--- Restaurante/src/app/services/reservas.services.ts
+++ Restaurante/src/app/services/reservas.services.ts
@@ -2,10 +2,7 @@ import { HttpClient, HttpHeaders, } from '@angular/common/http';
 import { Injectable, Observable } from '@angular/core';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
-import {  Reservas } from '../models/reserva';
-
-
-
+import { Reserva } from '../models/reserva';
 
 const httpOptions = {
     headers: new HttpHeaders({ 'Content-type': 'aplication/json' })
@@ -15,10 +12,7 @@ export class ReservasService {
     apiUrl: string;
     constructor(private http: HttpClient) { }
 
-
-
-    crearOActualizar(reservas: Reservas): Observable.Observable<Reservas[]> {
-        return this.http.post<Reservas[]>(API_URL_CREATE, reservas);
+    crearOActualizar(reservas: Reserva): Observable.Observable<Reserva[]> {
+        return this.http.post<Reserva[]>(API_URL_CREATE, reservas);
     }
-}
 }
